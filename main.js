59778ce801606b62b1fc5fc923dea63068382811
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-08 15:34:26 +0330

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const fs = require("fs");
+
+const readline = require("node:readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});

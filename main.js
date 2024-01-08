fcbb527a4622f0b7a06563514a95ce560f71e5db
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-08 15:35:58 +0330

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,38 @@ const getInput = (message) => {
   });
 };
 
+const getFollowers = async (username) => {
+  console.log("Data is extracting from github ...");
+  try {
+    finish_followers = false;
+    p_index = 1;
+    let followers_data = [];
+    while (!finish_followers) {
+      const response = await axios.get(
+        `https://github.com/${username}?page=${p_index}&tab=followers`
+      );
+
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      let follower_p_data = [];
+      if ($("span.Link--secondary").length == 0) {
+        finish_followers = true;
+      } else {
+        $("span.Link--secondary").each((i, person) =>
+          follower_p_data.push({
+            name: $(person).prev().text(),
+            username: $(person).text(),
+            profileLink: `https://github.com/${$(person).text()}`,
+          })
+        );
+      }
+      p_index++;
+      followers_data = await [...followers_data, ...follower_p_data];
+    }
+
+    return followers_data.length == 0 ? "user not found!" : followers_data;
+  } catch (error) {
+    throw error;
+  }
+};
